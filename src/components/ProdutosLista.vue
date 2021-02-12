fd30e80df9364e13a64c037c3a9070e43eae4a9e
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-12 09:05:51 -0300

    animações dos produtos

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,40 +1,48 @@
 <template>
   <section class="produtos-container">
-    <div v-if="produtos && produtos.length" class="produtos">
-      <div class="produto" v-for="(produto, index) in produtos" :key="index">
-        <router-link to="/">
-          <img
-            v-if="produto.fotos"
-            :src="produto.fotos[0].src"
-            :alt="produto.fotos[0].titulo"
-          />
-          <p class="preco">{{ produto.preco }}</p>
-          <h2 class="titulo">{{ produto.nome }}</h2>
-          <p>{{ produto.descricao }}</p>
-        </router-link>
+    <transition mode="out-in">
+      <div v-if="produtos && produtos.length" class="produtos" key="produtos">
+        <div class="produto" v-for="(produto, index) in produtos" :key="index">
+          <router-link to="/">
+            <img
+              v-if="produto.fotos"
+              :src="produto.fotos[0].src"
+              :alt="produto.fotos[0].titulo"
+            />
+            <p class="preco">{{ produto.preco }}</p>
+            <h2 class="titulo">{{ produto.nome }}</h2>
+            <p>{{ produto.descricao }}</p>
+          </router-link>
+        </div>
+        <ProdutosPaginar
+          :produtosTotal="produtosTotal"
+          :produtosPorPagina="produtosPorPagina"
+        />
       </div>
-      <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
-    </div>
-    <div v-else-if="produtos && produtos.length === 0">
-      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo</p>
-    </div>
+      <div v-else-if="produtos && produtos.length === 0" key="sem-resultados">
+        <p class="sem-resultados">
+          Busca sem resultados. Tente buscar outro termo
+        </p>
+      </div>
+      <PaginaCarregando key="carregando" v-else/>
+    </transition>
   </section>
 </template>
 
 <script>
-import ProdutosPaginar from '@/components/ProdutosPaginar.vue'
+import ProdutosPaginar from "@/components/ProdutosPaginar.vue";
 import { api } from "@/services.js";
 import { serialize } from "@/helpers.js";
 
 export default {
   name: "ProdutosLista",
   components: {
-    ProdutosPaginar
+    ProdutosPaginar,
   },
   data() {
     return {
       produtos: null,
-      produtosPorPagina: 3,
+      produtosPorPagina: 9,
       produtosTotal: 0,
     };
   },
@@ -46,10 +54,11 @@ export default {
   },
   methods: {
     getProdutos() {
-      api.get(this.url).then((json) => {
-        this.produtosTotal = Number(json.headers["x-total-count"]);
-        this.produtos = json.data;
-      });
+      this.produtos = null;
+        api.get(this.url).then((json) => {
+          this.produtosTotal = Number(json.headers["x-total-count"]);
+          this.produtos = json.data;
+        });
     },
   },
   watch: {
@@ -75,15 +84,15 @@ export default {
   margin: 30px;
 }
 .produto {
-  box-shadow: 0 4px 8px rgba(30, 60, 90, .1);
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
   padding: 10px;
   background: white;
   border-radius: 4px;
-  transition: all .2s;
+  transition: all 0.2s;
 }
 
 .produto:hover {
-  box-shadow: 0 6px 12px rgba(30, 60, 90, .1);
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.1);
   transform: scale(1.1);
   position: relative;
   z-index: 1;
